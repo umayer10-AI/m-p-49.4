0386e5ece4ddd81955d1345c9ce0db78adf9cd58
umayer10-AI <mdumayer10@gmail.com>
2026-05-05 13:31:28 +0600

    add

---
+++ src/app/about/new/page.jsx
@@ -0,0 +1,82 @@
+"use client";
+import {FloppyDisk} from "@gravity-ui/icons";
+import {
+  Button,
+  Description,
+  FieldError,
+  FieldGroup,
+  Fieldset,
+  Form,
+  Input,
+  Label,
+  TextArea,
+  TextField,
+} from "@heroui/react";
+import React from 'react';
+
+const page = () => {
+
+    const a = async (e) => {
+        e.preventDefault()
+
+        const formData = new FormData(e.target);
+        const newUser = Object.fromEntries(formData.entries())
+        console.log(newUser)
+
+        const res = await fetch("http://localhost:5000/about", {
+            method: "POST",
+            headers: {
+                "content-type" : "application/json"
+            },
+            body: JSON.stringify(newUser)
+        })
+
+        const data = await res.json()
+        console.log(data)
+
+    }
+
+    return (
+        <div>
+            <Form className="w-full max-w-96" onSubmit={a}>
+      <Fieldset>
+        <Fieldset.Legend>Profile Settings</Fieldset.Legend>
+        <Description>Update your profile information.</Description>
+        <FieldGroup>
+          <TextField
+            isRequired
+            name="name"
+            validate={(value) => {
+              if (value.length < 3) {
+                return "Name must be at least 3 characters";
+              }
+              return null;
+            }}
+          >
+            <Label>Name</Label>
+            <Input placeholder="John Doe" />
+            <FieldError />
+          </TextField>
+          <TextField isRequired name="email" type="email">
+            <Label>Email</Label>
+            <Input placeholder="john@example.com" />
+            <FieldError />
+          </TextField>
+          
+        </FieldGroup>
+        <Fieldset.Actions>
+          <Button type="submit">
+            <FloppyDisk />
+            Submit
+          </Button>
+          <Button type="reset" variant="secondary">
+            Cancel
+          </Button>
+        </Fieldset.Actions>
+      </Fieldset>
+    </Form>
+        </div>
+    );
+};
+
+export default page;
